abd8c6a2b47cf41d3af2064ea6b00851c66c8d43
darkpixels201 <darkpixels201@gmail.com>
2023-02-03 00:47:48 +0500

    jpg to png

--- mobile-app/app.config.js
+++ mobile-app/app.config.js
@@ -20,7 +20,7 @@ export default {
     orientation: "portrait",
     icon: "./assets/images/logo.png",
     splash: {
-        "image": "./assets/images/splash.jpg",
+        "image": "./assets/images/splash.png",
         "resizeMode": "cover",
         "backgroundColor": "#ffffff"
     },
